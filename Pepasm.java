352ecc2da168f30158c5b1e81be5be55e1bb229c
Narble <152878328+EvanDrake725@users.noreply.github.com>
2025-03-03 17:14:34 -0500

    Create Pepasm.java
    Beginning phases

---
+++ Pepasm.java
@@ -0,0 +1,8 @@
+public class Pepasm {
+    public static void main(String[] args){
+        //System.out.println("Hello world");
+        if(args.length>0){
+            System.out.print("args[0]= "+args[0]);
+        }
+    }
+}
