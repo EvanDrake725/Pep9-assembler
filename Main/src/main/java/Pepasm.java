03f1a46c00815e3be340d426e772dda642519707
Chharnish <reapsfrom2@gmail.com>
2025-03-07 22:15:11 -0500

    V0.3

--- Main/src/main/java/Pepasm.java
+++ Main/src/main/java/Pepasm.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Pepasm {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the file name (Do not include extensions): ");
         //opens file info
@@ -44,63 +44,63 @@ public class Pepasm {
         //checks "words" to change them
         for (int i = 0; i < assemblyWords.size(); i++) {
             String currWord = assemblyWords.get(i);
-            if (currWord.equals("STBA")) {
-                if (assemblyWords.get(i+2).equals("d")) {
-                    System.out.print("F1 ");
-                }else{System.out.print("F0 ");}
-            }
-            else if (currWord.equals("LDBA")) {
-                if (assemblyWords.get(i + 2).equals("d")) {
-                    System.out.print("D1 ");
-                } else {
-                    System.out.print("D0 ");
+            //checking to see if instruction starts with 0x (hexadecimal)
+            if (currWord.startsWith("0x")){
+                //removes the 0x from the word
+                currWord = currWord.substring(2);
+                //separates the string into 2 separate parts for each instruction
+                String[] memoryInstructions = {currWord.substring(0,2), currWord.substring(2)};
+                System.out.print(memoryInstructions[0] + " " + memoryInstructions[1]);
                 }
-            }
-            else if (currWord.equals("STWA")) {
-                if (assemblyWords.get(i + 2).equals("d")) {
-                    System.out.print("E1 ");
-                } else {
-                    System.out.print("E0 ");
+            switch (currWord) {
+                case "STBA" -> {
+                    if (assemblyWords.get(i + 2).equals("d")) {
+                        System.out.print("F1 ");
+                    } else {
+                        System.out.print("F0 ");
+                    }
                 }
-            }
-            else if (currWord.equals("LDWA")) {
-                if (assemblyWords.get(i + 2).equals("d")) {
-                    System.out.print("C1 ");
-                } else {
-                    System.out.print("C0 ");
+                case "LDBA" -> {
+                    if (assemblyWords.get(i + 2).equals("d")) {
+                        System.out.print("D1 ");
+                    } else {
+                        System.out.print("D0 ");
+                    }
                 }
-            }
-            else if (currWord.equals("ADDA")) {
-                if (assemblyWords.get(i + 2).equals("d")) {
-                    System.out.print("61 ");
-                } else {
-                    System.out.print("60 ");
+                case "STWA" -> {
+                    if (assemblyWords.get(i + 2).equals("d")) {
+                        System.out.print("E1 ");
+                    } else {
+                        System.out.print("E0 ");
+                    }
                 }
-            }
-            else if(currWord.equals("ASLA")){
-                System.out.print("0A ");
-            }
-            else if(currWord.equals("ASRA")){
-                System.out.print("0C ");
-            }
-            else if (currWord.equals("CPBA")) {
-                if (assemblyWords.get(i + 2).equals("d")) {
-                    System.out.print("B1 ");
-                } else {
-                    System.out.print("B0 ");
+                case "LDWA" -> {
+                    if (assemblyWords.get(i + 2).equals("d")) {
+                        System.out.print("C1 ");
+                    } else {
+                        System.out.print("C0 ");
+                    }
                 }
-            }
-            else if (currWord.equals("BRNE")) {
-                System.out.print("1A003 ");
+                case "ADDA" -> {
+                    if (assemblyWords.get(i + 2).equals("d")) {
+                        System.out.print("61 ");
+                    } else {
+                        System.out.print("60 ");
+                    }
                 }
-            else if(currWord.equals("STOP")){
-                System.out.print("00 ");
-            }
-            else if(currWord.equals(".END")){
-                System.out.print("zz ");
-            }
-            else {
-                System.out.print(currWord + " ");
+                case "ASLA" -> System.out.print("0A ");
+                case "ASRA" -> System.out.print("0C ");
+                case "CPBA" -> {
+                    if (assemblyWords.get(i + 2).equals("d")) {
+                        System.out.print("B1 ");
+                    } else {
+                        System.out.print("B0 ");
+                    }
+                }
+                case "BRNE" -> System.out.print("1A003 ");
+                case "STOP" -> System.out.print("00 ");
+                case ".END" -> System.out.print("zz ");
+                default -> System.out.print(" ");
             }
         }
     }
